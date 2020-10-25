db7ac00f688c3c2e78c42bc83e0d7a8da87dc294
mioyosss <bogdanstegny1@gmail.com>
2020-10-25 15:12:07 +0200

    lab03

---
+++ lab03/3/src/main.c
@@ -0,0 +1,13 @@
+
+int main()
+{
+	int a = 2; // Записано чило а
+	int a_v_kradrate = a * a; // Формула для отримання а^6
+	int a_v_4_stepeni = a_v_kradrate * a_v_kradrate; // Формула для отримання а^4
+	int a_v_6_stepeni = a_v_4_stepeni * a_v_kradrate; // Формула для отримання а^6
+	int a_v_8_stepeni = a_v_4_stepeni * a_v_4_stepeni; // Формула для отримання а^8
+	int a_v_10_stepeni = a_v_6_stepeni * a_v_4_stepeni; // Формула для отримання а^10
+	 
+
+	return 0;
+}
